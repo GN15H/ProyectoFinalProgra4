83a1a2dfcc5b09bd055b58f82375b9d2cc2d0b00
GN15H <cristobalochoacadavid@gmail.com>
2024-06-03 15:22:34 -0500

    cleaned main

--- ProyectoFinalPrograIV/src/main/ProyectoFinal.java
+++ ProyectoFinalPrograIV/src/main/ProyectoFinal.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.util.List;
 
 import views.LoginView;
 
@@ -10,16 +9,8 @@ public class ProyectoFinal {
 	
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		//UserController userController = new UserController();
-		//userController.addElement(new User("C.C", "1089097158", "Cristobal", "Ochoa", "root", "dir", "dir", "3202599542", "12345", UserType.admin));
 		LoginView loginView = new LoginView();
 	}
 	
-	public static <T> void printArray(List<T> array) {
-        for (T element : array) {
-            System.out.print(element.toString() + " ");
-        }
-        System.out.println();
-    }
 
 }
